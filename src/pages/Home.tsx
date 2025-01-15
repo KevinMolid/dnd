df73724c6c5f01dad12e529f2ec9bf1eb2914000
Kevin Molid <kevinmolid@gmail.com>
2025-01-15 19:54:52 +0100

    Laget Home med velkommen- og infotekst

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,21 +5,24 @@ const Home = () => {
   return (
     <div>
       <Container>
+        <H2>Velkommen!</H2>
         <p className="mb-4">
-          Velkommen! Denne siden er laget for å holde styr på alt som skjer i
-          DnD-eventyret "Den Tapte Gruven i Phandelver".
+          Denne siden er laget for å holde styr på alt som skjer i DnD-eventyret
+          <span className="font-medium"> Den Tapte Gruven i Phandelver</span>.
         </p>
         <p className="mb-4">
-          Her finner du en oppsummering av de viktigste hendelsene så langt. Du
-          finner de viktigste reglene du trenger som spiller og generelle tips
-          til hva man kan tenke på for å gjøre det til en morsom og positiv
-          opplevelse for alle!
+          Her finner du en oppsummering av de viktigste hendelsene i{" "}
+          <span className="font-medium">Eventyret</span> så langt. Du finner de
+          viktigste <span className="font-medium">Reglene</span> du trenger som
+          spiller og generelle{" "}
+          <span className="font-medium">Tips og Triks</span> til hva man kan
+          tenke på for å gjøre det til en morsom og positiv opplevelse for alle!
         </p>
-        <H2>Historien</H2>
-        <p>Session 1</p>
-        <H2>Reglene</H2>
-        <H2>Spillerne</H2>
-        <H2>NPCene</H2>
+        <p className="mb-4">
+          Du finner også informasjon om{" "}
+          <span className="font-medium">Spillerne</span> og{" "}
+          <span className="font-medium">NPCene</span>.
+        </p>
       </Container>
     </div>
   );
