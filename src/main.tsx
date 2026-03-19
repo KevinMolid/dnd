63a6cabf6b26aed8914c45f926b39cd2ca7c30cf
Kevin Molid <kevinmolid@gmail.com>
2026-03-19 08:22:12 +0100

    Fixed build

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { EncounterProvider } from "./context/EncounterContext";
-import { PartyInventoryProvider } from "./context/PartyInventoryContext";
-import { AuthProvider } from "./context/AuthContext";
+import { EncounterProvider } from "./context/EncounterContext.tsx";
+import { PartyInventoryProvider } from "./context/PartyInventoryContext.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
