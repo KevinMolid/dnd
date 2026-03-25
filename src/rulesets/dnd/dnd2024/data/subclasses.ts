991ef8eeb1662d265059a14d37d73871a88e76ca
Kevin Molid <kevinmolid@gmail.com>
2026-03-25 21:03:42 +0100

    Added missing clericSubclasses import/export

--- src/rulesets/dnd/dnd2024/data/subclasses.ts
+++ src/rulesets/dnd/dnd2024/data/subclasses.ts
@@ -1,5 +1,6 @@
 import { barbarianSubclasses } from "./classes/barbarian/subclasses";
 import { bardSubclasses } from "./classes/bard/subclasses";
+import { clericSubclasses } from "./classes/cleric";
 import { druidSubclasses } from "./classes/druid/subclasses";
 import { paladinSubclasses } from "./classes/paladin/subclasses";
 import { rogueSubclasses } from "./classes/rogue/subclasses";
@@ -8,6 +9,7 @@ import { rogueSubclasses } from "./classes/rogue/subclasses";
 export const subclasses = [
   ...barbarianSubclasses,
   ...bardSubclasses,
+  ...clericSubclasses,
   ...druidSubclasses,
   ...paladinSubclasses,
   ...rogueSubclasses,
