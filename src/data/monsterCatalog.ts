09868f784e8e0455d3a28bca2fabb5343110d846
Kevin Molid <kevinmolid@gmail.com>
2026-04-01 19:22:51 +0200

    Added Banshee

--- src/data/monsterCatalog.ts
+++ src/data/monsterCatalog.ts
@@ -81,6 +81,52 @@ export const monsterTypes: MonsterType[] = [
 
 export const monsters: MonsterDefinition[] = [
   {
+    id: "banshee",
+    name: "Banshee",
+    type: "Undead",
+    description: "Medium Undead, Chaotic Evil",
+    img: "/Monsters/Banshee.png",
+    armorClass: 12,
+    hp: 58,
+    speed: "0 ft., fly 40 ft. (hover)",
+    stats: {
+      str: 1,
+      dex: 14,
+      con: 10,
+      int: 12,
+      wis: 11,
+      cha: 17,
+    },
+    senses: "Darkvision 60 ft., passive Perception 10",
+    language: "Common, Elvish",
+    challengeRating: "4",
+    xp: 1100,
+    traits: [
+      {
+        name: "Detect Life",
+        text: "The banshee can magically sense the presence of creatures up to 5 miles away that aren’t undead or constructs. She knows the general direction they’re in but not their exact locations.",
+      },
+      {
+        name: "Incorporeal Movement",
+        text: "The banshee can move through other creatures and objects as if they were difficult terrain. She takes 5 (1d10) force damage if she ends her turn inside an object.",
+      },
+    ],
+    actions: [
+      {
+        name: "Corrupting Touch",
+        text: "Melee Spell Attack: +4 to hit, reach 5 ft., one target. Hit: 12 (3d6 + 2) necrotic damage.",
+      },
+      {
+        name: "Horrifying Visage",
+        text: "Each non-undead creature within 60 feet of the banshee that can see her must succeed on a DC 13 Wisdom saving throw or be frightened for 1 minute. A frightened target can repeat the saving throw at the end of each of its turns, with disadvantage if the banshee is within line of sight, ending the effect on itself on a success. If a target’s saving throw is successful or the effect ends for it, the target is immune to the banshee’s Horrifying Visage for the next 24 hours.",
+      },
+      {
+        name: "Wail (1/Day)",
+        text: "The banshee releases a mournful wail, provided that she isn’t in sunlight. This wail has no effect on constructs and undead. All other creatures within 30 feet of her that can hear her must make a DC 13 Constitution saving throw. On a failure, a creature drops to 0 hit points. On a success, a creature takes 10 (3d6) psychic damage.",
+      },
+    ],
+  },
+  {
     id: "bugbear",
     name: "Bugbear",
     type: "Humanoid",
